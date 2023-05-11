2ed61b6c5a3e79756ecd34bad34137c6817e5255
Bibin A <bibinolavanna@gmail.com>
2023-05-11 07:39:13 +0530

    code clean up

--- controllers/admin/works.js
+++ controllers/admin/works.js
@@ -3,7 +3,7 @@ const bookingModel = require("../../models/user/booking");
 exports.viewWorks = async (req, res) => {
   try {
     const works = await bookingModel
-      .find({PhotographerPayment:false})
+      .find({ PhotographerPayment: false })
       .populate("user")
       .populate("company")
       .sort({ _id: -1 });
@@ -14,7 +14,7 @@ exports.viewWorks = async (req, res) => {
 };
 
 exports.photographerPayment = async (req, res) => {
-  try {  
+  try {
     const id = req.params.id;
     const amount = req.body.amount;
     await bookingModel.findByIdAndUpdate(id, {
@@ -26,24 +26,28 @@ exports.photographerPayment = async (req, res) => {
   }
 };
 
-exports.history=async(req,res)=>{
-    try {
-        // const list = await bookingModel.find({$or:[{photographerPayment:true},{status:"Cancelled"}]}).populate("company").populate("user").sort({_id:-1})
-        const list = await bookingModel.find({PhotographerPayment:true}).populate("company").populate("user").sort({_id:-1})
-        res.status(200).json({data:list})
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-exports.sales=async(req,res)=>{
+exports.history = async (req, res) => {
   try {
-    const {from ,to}= req.body
-    const data = await bookingModel.find({PhotographerPayment:true,updatedAt:{$gte:from,$lt:to}}).populate('company').sort({_id:-1})
-    // const total = await bookingModel.aggregate([{$match:{$and:[{PhotographerPayment:true,updatedAt:{$gte:from,$lt:to}}]}},{$group:{_id:1}}])
-    // console.log(total)
-    res.status(200).json({data:data})
+    const list = await bookingModel
+      .find({ PhotographerPayment: true })
+      .populate("company")
+      .populate("user")
+      .sort({ _id: -1 });
+    res.status(200).json({ data: list });
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
+
+exports.sales = async (req, res) => {
+  try {
+    const { from, to } = req.body;
+    const data = await bookingModel
+      .find({ PhotographerPayment: true, updatedAt: { $gte: from, $lt: to } })
+      .populate("company")
+      .sort({ _id: -1 });
+    res.status(200).json({ data: data });
+  } catch (error) {
+    console.log(error);
+  }
+};
